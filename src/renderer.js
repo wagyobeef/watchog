fc65c2413f70c1535ef41169e0e535972fd77d06
wagyobeef <grant.jj.kim@gmail.com>
2026-02-05 19:17:25 -0800

    integrate react

--- src/renderer.js
+++ src/renderer.js
@@ -27,6 +27,7 @@
  */
 
 import './index.css';
+import './app.jsx';
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack',
--- webpack.rules.js
+++ webpack.rules.js
@@ -32,4 +32,14 @@ module.exports = [
    *   }]
    * }
    */
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        exclude: /node_modules/,
+        presets: ['@babel/preset-react']
+      }
+    }
+  }
 ];
